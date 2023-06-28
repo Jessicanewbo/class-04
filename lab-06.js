ca019e8220ce1620f66318836c409c1eb1289240
JessicaNewbo <jessica.abdoney@iowastartupacclerator.com>
2023-06-28 15:42:02 -0500

    lab-o6.js

---
+++ lab-06.js
@@ -0,0 +1,27 @@
+"use strict"; 
+
+console.log ('hello!!');
+
+let allergic;
+
+allergic = prompt('Are you allergic to flowers?');
+
+console.log(allergic);
+if(allergic ==='yes'){
+    alert('stay away from us');
+    confirm('you are allergic to flowers?');
+
+} else if(allergic ==='no') { 
+    alert('good for you');
+}
+
+
+
+else{ 
+    alert('FIGURE IT OUT')
+}
+
+
+
+
+
