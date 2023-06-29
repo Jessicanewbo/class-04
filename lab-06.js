cadecd87c00f36ffff6832115880d23a3a4303b1
JessicaNewbo <jessica.abdoney@iowastartupacclerator.com>
2023-06-29 15:37:46 -0500

    lab-06

--- lab-06.js
+++ lab-06.js
@@ -16,12 +16,40 @@ let allergicResponse = function () {
     } else {
         alert('FIGURE IT OUT')
     }
-    return document.write('have a nice day'); 
+    return document.write('have a nice day');
 } //closes function 
 
 
+function TaylorSwiftfavoritenumber() {
+    let answer;
+    while (answer != 13) {
+        answer = prompt('Whats Taylor Swift Lucky Number?');
+        if (answer != 13) {
+            alert('try again');
+        } else {
+            alert('you are the best fan!');
+        }
+
+    }
+}
+TaylorSwiftfavoritenumber();
 
 
+function displayrating() {
+    let output = '';
+    let Midnight = prompt('scale of 1 to 3 would you rate Midnight');
+    for (let i = 0; i < Midnight; i++) {
+        console.log(i);
+        output += '<img src="https://upload.wikimedia.org/wikipedia/en/9/9f/Midnights_-_Taylor_Swift.png" height="250px" width="250px"  />';
+    } 
+
+    return document.write(output);
+}
+
+
+
+
+displayrating();
 
 
 
